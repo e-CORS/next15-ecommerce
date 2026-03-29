8fb687252f086fb3fd38d2a3ec47bbc3523fa4bd
Enzo <ecorrales@spacedev.uy>
2026-03-29 17:14:43 +0200

    add: use Product card component on home page

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-gray-200 rounded-lg p-4" key={product.id}>
+    <div className="border border-gray-200 rounded-lg p-4">
       <Image
         src={product.image}
         alt={product.name}
         width={300}
         height={300}
+        loading="eager"
         className="w-full h-48 object-cover object-center rounded-lg mb-4"
       />
       <h2 className="text-lg font-semibold">{product.name}</h2>
