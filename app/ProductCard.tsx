9194f24286a2c966e70c9bde528de2c8dfa6e908
Enzo Corrales <86249110+e-CORS@users.noreply.github.com>
2026-03-29 17:20:48 +0200

    [Feat] Add product card component (#2)
    * add: define the ProductCarc component
    
    * add: improvements to ProductCard
    
    * add: whitelist for domain images.unsplash for mock images
    
    * add: use Product card component on home page
    
    ---------
    
    Co-authored-by: Enzo <ecorrales@spacedev.uy>

---
+++ app/ProductCard.tsx
@@ -0,0 +1,20 @@
+import { Product } from "@/lib/mocks";
+import Image from "next/image";
+
+export function ProductCard({ product }: { product: Product }) {
+  return (
+    <div className="border border-gray-200 rounded-lg p-4">
+      <Image
+        src={product.image}
+        alt={product.name}
+        width={300}
+        height={300}
+        loading="eager"
+        className="w-full h-48 object-cover object-center rounded-lg mb-4"
+      />
+      <h2 className="text-lg font-semibold">{product.name}</h2>
+      <p className="text-gray-600">${product.price.toFixed(2)}</p>
+      <p className="text-gray-500">{product.description}</p>
+    </div>
+  );
+}
