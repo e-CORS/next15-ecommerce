d7267b8929422c61246f262681a1e5e587f343ec
Enzo Corrales <86249110+e-CORS@users.noreply.github.com>
2026-03-29 19:16:54 +0200

    [Feat] Consume data from prisma (#5)
    * add: consume products from prisma
    
    * remove: mocks file
    
    ---------
    
    Co-authored-by: Enzo <ecorrales@spacedev.uy>

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -1,21 +1,24 @@
-import { Product } from "@/lib/mocks";
+import Image from "next/image";
+
 import { formatPrice } from "@/lib/utils";
 
-import Image from "next/image";
+import type { Product } from "./generated/prisma/client";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
-      <div className="relative overflow-hidden aspect-video mb-4">
-        <Image
-          src={product.image}
-          alt={product.name}
-          fill
-          loading="eager"
-          className="object-cover rounded-lg"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
-      </div>
+      {product.image && (
+        <div className="relative overflow-hidden aspect-video mb-4">
+          <Image
+            src={product.image}
+            alt={product.name}
+            fill
+            loading="eager"
+            className="object-cover rounded-lg"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+      )}
       <h2 className="text-lg font-semibold">{product.name}</h2>
       <p className="text-gray-600">{formatPrice(product.price)}</p>
       <p className="text-gray-500">{product.description}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { mockProducts } from "@/lib/mocks";
 import { ProductCard } from "./ProductCard";
+import { prisma } from "@/lib/prisma";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const products = await prisma.product.findMany();
+
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Home</h1>
-      <p className="text-gray-600 mb-4">
-        Showing {mockProducts.length} products
-      </p>
+      <p className="text-gray-600 mb-4">Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {mockProducts.map((product) => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
