7abaa13402158fde65c11b09dc211668e0e3b70b
Enzo <ecorrales@spacedev.uy>
2026-04-05 15:41:15 +0200

    add: image to product page and update loading state

--- app/product/[slug]/loading.tsx
+++ app/product/[slug]/loading.tsx
@@ -7,20 +7,23 @@ export default function Loading() {
   return (
     <main className="container mx-auto p-4">
       <Card className="max-w-3xl mx-auto">
-        <CardContent className="p-6 space-y-4">
-          <Skeleton className="w-1/2 h-8" />
+        <CardContent className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <Skeleton className="w-full h-84 aspect-square" />
+          <div className="space-y-4">
+            <Skeleton className="w-1/2 h-8" />
 
-          <div className="flex items-center gap-2 mb-4">
-            <Skeleton className="w-1/8 h-6" />
-            <Skeleton className="w-1/8 h-6" />
-          </div>
+            <div className="flex items-center gap-2 mb-4">
+              <Skeleton className="w-1/4 h-6" />
+              <Skeleton className="w-1/4 h-6" />
+            </div>
 
-          <Separator className="my-4" />
+            <Separator className="my-4" />
 
-          <div className="space-y-2">
-            <h2 className="font-medium">Description</h2>
-            <Skeleton className="w-full h-4" />
-            <Skeleton className="w-full h-4" />
+            <div className="space-y-2">
+              <h2 className="font-medium">Description</h2>
+              <Skeleton className="w-full h-4" />
+              <Skeleton className="w-1/2 h-4" />
+            </div>
           </div>
         </CardContent>
       </Card>
--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { CardContent, Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { getProductBySlug } from "@/lib/actions";
 import { formatPrice, sleep } from "@/lib/utils";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({
@@ -45,18 +46,33 @@ export default async function ProductPage({
   return (
     <main className="container mx-auto p-4">
       <Card className="max-w-3xl mx-auto">
-        <CardContent className="p-6">
-          <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
-          <div className="flex items-center gap-2 mb-4">
-            <span className="font-semibold text-lg">
-              {formatPrice(product.price)}
-            </span>
-            <Badge variant="outline">{product.category?.name}</Badge>
-          </div>
-          <Separator className="my-4" />
-          <div className="space-y-2">
-            <h2 className="font-medium">Description</h2>
-            <p className="font-light text-gray-600">{product.description}</p>
+        <CardContent className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+          {product.image && (
+            <div className="relative rounded-lg overflow-hidden aspect-square mb-2">
+              <Image
+                src={product.image}
+                alt={product.name}
+                fill
+                priority
+                loading="eager"
+                className="object-cover "
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+              />
+            </div>
+          )}
+          <div>
+            <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+            <div className="flex items-center gap-2 mb-4">
+              <span className="font-semibold text-lg">
+                {formatPrice(product.price)}
+              </span>
+              <Badge variant="outline">{product.category?.name}</Badge>
+            </div>
+            <Separator className="my-4" />
+            <div className="space-y-2">
+              <h2 className="font-medium">Description</h2>
+              <p className="font-light text-gray-600">{product.description}</p>
+            </div>
           </div>
         </CardContent>
       </Card>
