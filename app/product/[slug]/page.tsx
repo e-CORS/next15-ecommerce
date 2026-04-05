2d9fdd647e1d7e436e5c300b06bfccbce2c519cc
Enzo <ecorrales@spacedev.uy>
2026-04-05 15:19:22 +0200

    add: improve Product page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { Badge } from "@/components/ui/badge";
+import { CardContent, Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { getProductBySlug } from "@/lib/actions";
 import { formatPrice } from "@/lib/utils";
 import { notFound } from "next/navigation";
@@ -15,10 +18,23 @@ export default async function ProductPage({
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">{product.name}</h1>
-      <p className="text-gray-600 mb-6">{product.description}</p>
-      <p className="text-gray-600 mb-6">{formatPrice(product.price)}</p>
-    </div>
+    <main className="container mx-auto p-4">
+      <Card className="max-w-3xl mx-auto">
+        <CardContent className="p-6">
+          <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+          <div className="flex items-center gap-2 mb-4">
+            <span className="font-semibold text-lg">
+              {formatPrice(product.price)}
+            </span>
+            <Badge variant="outline">{product.category?.name}</Badge>
+          </div>
+          <Separator className="my-4" />
+          <div className="space-y-2">
+            <h2 className="font-medium">Description</h2>
+            <p className="font-light text-gray-600">{product.description}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
