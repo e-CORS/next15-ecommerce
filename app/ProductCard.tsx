0906baf02639c7e1a7c49c72e3c0e42aa934a719
Enzo Corrales <86249110+e-CORS@users.noreply.github.com>
2026-04-05 15:07:15 +0200

    [Feat] Add dynamic routes to view specific products  (#9)
    * add: server actions
    
    * add: product page and handle navigation from product card
    
    ---------
    
    Co-authored-by: Enzo <ecorrales@spacedev.uy>

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 import { formatPrice } from "@/lib/utils";
 
@@ -13,26 +14,28 @@ import type { Product } from "./generated/prisma/client";
 
 export function ProductCard({ product }: { product: Product }) {
   return (
-    <Card className="pt-0 overflow-hidden">
-      {product.image && (
-        <div className="relative overflow-hidden aspect-video mb-2">
-          <Image
-            src={product.image}
-            alt={product.name}
-            fill
-            loading="eager"
-            className="object-cover rounded-lg rounded-b-none"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-      )}
-      <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <p className="text-gray-600">{formatPrice(product.price)}</p>
-      </CardFooter>
-    </Card>
+    <Link href={`/product/${product.slug}`}>
+      <Card className="pt-0 overflow-hidden">
+        {product.image && (
+          <div className="relative overflow-hidden aspect-video mb-2">
+            <Image
+              src={product.image}
+              alt={product.name}
+              fill
+              loading="eager"
+              className="object-cover rounded-lg rounded-b-none"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          </div>
+        )}
+        <CardHeader>
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription>{product.description}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <p className="text-gray-600">{formatPrice(product.price)}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { getProductBySlug } from "@/lib/actions";
+import { formatPrice } from "@/lib/utils";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">{product.name}</h1>
+      <p className="text-gray-600 mb-6">{product.description}</p>
+      <p className="text-gray-600 mb-6">{formatPrice(product.price)}</p>
+    </div>
+  );
+}
