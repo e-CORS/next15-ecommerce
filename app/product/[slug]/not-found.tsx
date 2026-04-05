0f27e7952944ebe110dfef2a54750ebf78c44bd2
Enzo Corrales <86249110+e-CORS@users.noreply.github.com>
2026-04-05 15:10:44 +0200

    add: custom not found page (#10)
    Co-authored-by: Enzo <ecorrales@spacedev.uy>

---
+++ app/product/[slug]/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Product not found</h1>
+      <p className="text-gray-600 mb-6">
+        The product you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
